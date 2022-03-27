1994060f6e8d8e3d0c7da8432798ac310f88c198
Yudi Alviska <yudi.a@weefer.co.id>
2022-03-27 17:29:17 +0700

    initial commit

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [process.env.ENTITY_PATH],
+  synchronize: true,
+}
