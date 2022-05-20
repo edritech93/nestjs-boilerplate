4e86433fba0c3766e65f29a3900e1038f36d610b
Yudi Alviska <yudi.a@weefer.co.id>
2022-05-21 00:27:30 +0700

    add some package, rename folder user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './user_/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
