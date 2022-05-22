36f97d828b4cb3efc7db1551caa787b9e5846b55
Yudi Alviska <yudi.a@weefer.co.id>
2022-05-22 18:04:50 +0700

    add ConfigModule, remove unused file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { JwtStrategy } from './helper/jwt.strategy';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
-  controllers: [AppController],
-  providers: [AppService, JwtStrategy],
+  imports: [TypeOrmModule.forRoot(), ConfigModule.forRoot(), UsersModule],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
