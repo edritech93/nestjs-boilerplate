8f2997982377665a8c23874973553d199bd03dd0
Yudi Alviska <yudi.a@weefer.co.id>
2024-04-24 16:26:56 +0700

    rename folder entity users

--- src/users/service/users.service.ts
+++ src/users/service/users.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
-import { Users } from '../entities/users.entity';
+import { Users } from '../entity/users.entity';
 
 @Injectable()
 export class UsersService {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './service/users.service';
 import { UsersController } from './controller/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Users } from './entities/users.entity';
+import { Users } from './entity/users.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from 'src/libs/constants';
 
